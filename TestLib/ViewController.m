ab2421287fadba5d69820dc78a3680420fe4ceca
yushuhui <718780261@qq.com>
2017-11-10 15:06:38 +0800

    a

---
+++ TestLib/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestLib
+//
+//  Created by jingruijiaoyu on 2017/11/10.
+//  Copyright © 2017年 jingruijiaoyu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ TestLib/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestLib
+//
+//  Created by jingruijiaoyu on 2017/11/10.
+//  Copyright © 2017年 jingruijiaoyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
